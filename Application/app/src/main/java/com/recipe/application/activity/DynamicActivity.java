82db4a741f286db1ce2be4079f02fdd4eef3cbf6
lyy-dzq <82194690+lyy-dzq@users.noreply.github.com>
2022-04-21 11:18:48 +0800

    图片

---
+++ Application/app/src/main/java/com/recipe/application/activity/DynamicActivity.java
@@ -0,0 +1,16 @@
+package com.recipe.application.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.recipe.application.R;
+
+public class DynamicActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dynamic);
+    }
+}
