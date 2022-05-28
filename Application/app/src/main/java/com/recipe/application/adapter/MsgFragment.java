cb4e40b3514a6d369faba8c8f436baca4e20be48
lijie <lijie@gmail.com>
2022-05-28 22:42:55 +0800

    搜索页面设计

--- Application/app/src/main/java/com/recipe/application/adapter/MsgFragment.java
+++ Application/app/src/main/java/com/recipe/application/adapter/MsgFragment.java
@@ -1,6 +1,39 @@
 package com.recipe.application.adapter;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SearchView;
+import android.widget.TextView;
+
+
 import androidx.fragment.app.Fragment;
 
+import com.recipe.application.R;
+import com.recipe.application.activity.ShowActivity;
+import com.recipe.application.activity.SortActivity;
+import com.recipe.application.adapter.SortListAdapter;
+import com.recipe.application.dao.Brief_dish;
+import com.recipe.application.dao.Post;
+import com.recipe.application.utils.BitmapUtils;
+import com.recipe.application.utils.CameraUtils;
+import com.recipe.application.utils.SPUtils;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.material.imageview.ShapeableImageView;
+
+import java.util.List;
+import butterknife.ButterKnife;
+
+
 public class MsgFragment extends Fragment {
+   
 }
